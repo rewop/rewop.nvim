a9a60707cf89ec6960180acee894dc4e1327be38
Simone Potenza <simone.potenza@gmail.com>
2023-11-26 15:29:19 +0100

    chore: instaleed minipairs, minisurround and othr plugin config

--- lua/rewop/plugins/nvim-cmp.lua
+++ lua/rewop/plugins/nvim-cmp.lua
@@ -47,26 +47,26 @@ return  {
                     nehaviour = cmp.ConfirmBehavior.Replace,
                     select = true, 
                 }), 
-                -- ['<Tab>'] = cmp.mapping(function(fallback)
-                --
-                --     if cmp.visible() then
-                --         cmp.select_next_item()
-                --     elseif luasnip.expand_or_locally_jumpable() then
-                --         luasnip.expand_or_jump()
-                --     else
-                --         fallback()
-                --     end
-                --
-                -- end),
-                -- ['<S-Tab>'] = cmp.mapping(function(fallback)
-                --     if cmp.visible() then
-                --         cmp.select_prev_item()
-                --     elseif luasnip.locally_jumpable(-1) then
-                --         luasnip.jump(-1)
-                --     else
-                --         fallback()
-                --     end
-                -- end),
+                ['<Tab>'] = cmp.mapping(function(fallback)
+
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+
+                end),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lua' },
--- lua/rewop/plugins/telescope.lua
+++ lua/rewop/plugins/telescope.lua
@@ -1,20 +1,55 @@
-  -- Fuzzy Finder (files, lsp, etc)
-  return {
+-- Fuzzy Finder (files, lsp, etc)
+return {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
+        'nvim-lua/plenary.nvim',
+        -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+        -- Only load if `make` is available. Make sure you have the system
+        -- requirements installed.
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            -- NOTE: If you are having trouble with this installation,
+            --       refer to the README for telescope-fzf-native for more instructions.
+            build = 'make',
+            cond = function()
+                return vim.fn.executable 'make' == 1
+            end,
+        },
     },
-  }
+    config = function() 
+        require('telescope').setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-h>"] = "which_key",
+                    }
+                },
+                -- pickers = {
+                -- },   
+                -- extensions = {
+                -- }
+            }
+        })
+        -- add telescope pickers keys
+        telescope_builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader><space>', telescope_builtin.find_files, { desc = '[ ] Find existing buffers' })
+        vim.keymap.set('n', '<leader>?', telescope_builtin.oldfiles, { desc = '[?] Find recently opened files' })
+        vim.keymap.set('n', '<leader>/', function()
+            telescope_builtin.current_buffer_fuzzy_find({
+                previewer = false,
+                winblend = 10,
+            })
+        end, { desc = '[/] Fuzzily search in current buffer' })
+
+        vim.keymap.set('n', '<leader>gf', telescope_builtin.git_files, { desc = 'Search [G]it [F]ile' })
+        vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [F]ile' })
+        vim.keymap.set('n', '<leader>sb', telescope_builtin.buffers, { desc = '[S]earch [B]uffers' })
+        vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [H]elp' })
+        vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string, { desc = '[S]earch current [W]ord' })
+        vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>sd', telescope_builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>sr', telescope_builtin.resume, { desc = '[S]earch [R]esume' })
+        vim.keymap.set('n', '<leader>sk', telescope_builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+    end
+}
