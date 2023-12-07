8d5daa6c443d128cead5a4581f6ba7b361c6ca68
Simone Potenza <simone.potenza@gmail.com>
2023-12-07 19:41:47 +0100

    git: added keymaps and configured nvim-cmp

--- lua/rewop/plugins/nvim-cmp.lua
+++ lua/rewop/plugins/nvim-cmp.lua
@@ -3,38 +3,47 @@ return  {
     'hrsh7th/nvim-cmp',
     dependencies = {
         -- Snippet Engine & its associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-        'hrsh7th/cmp-path',
+        -- 'L3MON4D3/LuaSnip',
+        -- 'saadparwaiz1/cmp_luasnip',
+        --
+        'onsails/lspkind.nvim',
 
         -- Adds LSP completion capabilities
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-nvim-lua',
         "hrsh7th/cmp-path",
-
-        -- Adds a number of user-friendly snippets
-        'rafamadriz/friendly-snippets',
+        "hrsh7th/cmp-buffer",
     },
     config = function()
         local cmp = require("cmp")
-        local luasnip = require("luasnip")
-        
-        require("luasnip.loaders.from_vscode").lazy_load()
-        luasnip.config.setup({})
+        local lspkind = require("lspkind")
+        -- local luasnip = require("luasnip")
 
         cmp.setup({
-            snippet = {
-                -- REQUIRED - you must specify a snippet engine
-                expand = function(args)
-                    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                    luasnip.lsp_expand(args.body) -- For `luasnip` users.
-                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                end,
-            },
+            -- snippet = {
+            --     -- REQUIRED - you must specify a snippet engine
+            --     expand = function(args)
+            --         -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            --         luasnip.lsp_expand(args.body) -- For `luasnip` users.
+            --         -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            --         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            --     end,
+            -- },
             window = {
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
+            },
+            formatting = {
+                format = lspkind.cmp_format({
+                    mode = "symbol_text",
+                    menu = ({
+                        buffer = "[Buf]",
+                        nvim_lsp = "[LSP]",
+                        luasnip = "[LuaSnip]",
+                        nvim_lua = "[Lua]",
+                        path = "[Path]",
+                    }),
+                }),
             },
             mapping = cmp.mapping.preset.insert({
                 ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -48,11 +57,10 @@ return  {
                     select = true, 
                 }), 
                 ['<Tab>'] = cmp.mapping(function(fallback)
-
                     if cmp.visible() then
                         cmp.select_next_item()
-                    elseif luasnip.expand_or_locally_jumpable() then
-                        luasnip.expand_or_jump()
+                    -- elseif luasnip.expand_or_locally_jumpable() then
+                    --     luasnip.expand_or_jump()
                     else
                         fallback()
                     end
@@ -61,8 +69,8 @@ return  {
                 ['<S-Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
+                    -- elseif luasnip.locally_jumpable(-1) then
+                    --     luasnip.jump(-1)
                     else
                         fallback()
                     end
@@ -72,13 +80,13 @@ return  {
                 { name = 'nvim_lua' },
                 { name = 'path' },
                 { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
+                -- { name = 'luasnip' }, -- For luasnip users.
             }, {
                 { name = 'buffer' }, -- complete text from the current buffer
             })
         })
-
         -- Set configuration for specific filetype.
+        cmp.setup.filetype('lua')
         -- cmp.setup.filetype('gitcommit', {
         --     sources = cmp.config.sources({
         --         { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
@@ -86,7 +94,7 @@ return  {
         --         { name = 'buffer' },
         --     })
         -- })
-        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        -- -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
         -- cmp.setup.cmdline({ '/', '?' }, {
         --     mapping = cmp.mapping.preset.cmdline(),
         --     sources = {
@@ -95,13 +103,13 @@ return  {
         -- })
 
         -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    --     cmp.setup.cmdline(':', {
-    --         mapping = cmp.mapping.preset.cmdline(),
-    --         sources = cmp.config.sources({
-    --             { name = 'path' }
-    --         }, {
-    --             { name = 'cmdline' }
-    --         })
-    --     })
+        --     cmp.setup.cmdline(':', {
+        --         mapping = cmp.mapping.preset.cmdline(),
+        --         sources = cmp.config.sources({
+        --             { name = 'path' }
+        --         }, {
+        --             { name = 'cmdline' }
+        --         })
+        --     })
     end
 }
