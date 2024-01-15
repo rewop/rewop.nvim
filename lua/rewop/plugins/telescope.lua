b4383a9eb72dcc3b8f360e3b9cd0c4c6d8e2a2b0
Simone Potenza <simone.potenza@gmail.com>
2024-01-15 12:35:32 +0100

    changed telescope layout to vertical

--- lua/rewop/plugins/telescope.lua
+++ lua/rewop/plugins/telescope.lua
@@ -1,64 +1,66 @@
 -- Fuzzy Finder (files, lsp, etc)
 return {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-        -- Only load if `make` is available. Make sure you have the system
-        -- requirements installed.
-        {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            -- NOTE: If you are having trouble with this installation,
-            --       refer to the README for telescope-fzf-native for more instructions.
-            build = 'make',
-            cond = function()
-                return vim.fn.executable 'make' == 1
-            end,
-        },
-        "nvim-tree/nvim-web-devicons",
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+    -- Only load if `make` is available. Make sure you have the system
+    -- requirements installed.
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      -- NOTE: If you are having trouble with this installation,
+      --       refer to the README for telescope-fzf-native for more instructions.
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
     },
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
-        local telescope_builtin = require('telescope.builtin')
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    local telescope = require 'telescope'
+    local actions = require 'telescope.actions'
+    local telescope_builtin = require 'telescope.builtin'
 
-        telescope.setup({
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<C-h>"] = "which_key",
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-j>"] = actions.move_selection_next,
-                    },
-                },
-                -- pickers = {
-                -- },   
-                -- extensions = {
-                -- }
-            }
-        })
+    telescope.setup {
+      defaults = {
+        mappings = {
+          i = {
+            ['<C-h>'] = 'which_key',
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<C-j>'] = actions.move_selection_next,
+          },
+        },
+        layout_strategy = 'vertical',
+        layout_config = { height = 0.95, width = 0.95 },
+        -- pickers = {
+        -- },
+        -- extensions = {
+        -- }
+      },
+    }
 
-        telescope.load_extension("fzf")
+    telescope.load_extension 'fzf'
 
-        -- add telescope pickers keys
-        vim.keymap.set('n', '<leader><space>', telescope_builtin.find_files, { desc = '[ ] Find existing buffers' })
-        vim.keymap.set('n', '<leader>?', telescope_builtin.oldfiles, { desc = '[?] Find recently opened files' })
-        vim.keymap.set('n', '<leader>/', function()
-            telescope_builtin.current_buffer_fuzzy_find({
-                previewer = false,
-                winblend = 10,
-            })
-        end, { desc = '[/] Fuzzily search in current buffer' })
+    -- add telescope pickers keys
+    vim.keymap.set('n', '<leader><space>', telescope_builtin.find_files, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>?', telescope_builtin.oldfiles, { desc = '[?] Find recently opened files' })
+    vim.keymap.set('n', '<leader>/', function()
+      telescope_builtin.current_buffer_fuzzy_find {
+        previewer = false,
+        winblend = 10,
+      }
+    end, { desc = '[/] Fuzzily search in current buffer' })
 
-        vim.keymap.set('n', '<leader>gf', telescope_builtin.git_files, { desc = 'Search [G]it [F]ile' })
-        vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [F]ile' })
-        vim.keymap.set('n', '<leader>sb', telescope_builtin.buffers, { desc = '[S]earch [B]uffers' })
-        vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sd', telescope_builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader>sr', telescope_builtin.resume, { desc = '[S]earch [R]esume' })
-        vim.keymap.set('n', '<leader>sk', telescope_builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    end
+    vim.keymap.set('n', '<leader>gf', telescope_builtin.git_files, { desc = 'Search [G]it [F]ile' })
+    vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [F]ile' })
+    vim.keymap.set('n', '<leader>sb', telescope_builtin.buffers, { desc = '[S]earch [B]uffers' })
+    vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sd', telescope_builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sr', telescope_builtin.resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>sk', telescope_builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+  end,
 }
