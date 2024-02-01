f0887a2f26760749734b4ee92a0bf1ae7c880652
Simone Potenza <simone.potenza@gmail.com>
2024-02-01 23:42:36 +0100

    add gitlinker keymaps

--- lua/rewop/plugins/gitlinker.lua
+++ lua/rewop/plugins/gitlinker.lua
@@ -4,10 +4,18 @@ return {
   config = function()
     local gl = require 'gitlinker'
     local gl_actions = require 'gitlinker.actions'
-    gl.setup()
+    gl.setup {
+      mappings = nil,
+    }
 
     local vk = vim.keymap.set
 
+    vk('n', '<leader>gy', function()
+      gl.get_buf_range_url('n', { action_callback = gl_actions.copy_to_clipboard })
+    end, { silent = true, desc = 'Copy permalink' })
+    vk('v', '<leader>gy', function()
+      gl.get_buf_range_url('v', { action_callback = gl_actions.copy_to_clipboard })
+    end, { silent = true, desc = 'Copy permalink' })
     vk('n', '<leader>gP', function()
       gl.get_buf_range_url('n', { action_callback = gl_actions.open_in_browser })
     end, { silent = true, desc = 'Open permalink in browser' })
