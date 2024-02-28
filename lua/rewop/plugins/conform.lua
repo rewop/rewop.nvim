48bdfaf0ab8aef2a2a214991918261cec43086bc
Simone Potenza <simone.potenza@gmail.com>
2024-02-28 09:07:15 +0100

    setup conform plugin

--- lua/rewop/plugins/conform.lua
+++ lua/rewop/plugins/conform.lua
@@ -1,3 +1,24 @@
+-- Create command to be able to disable autoformatiing on save
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+
+-- Create command to enable formatting on save
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
 return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
@@ -29,7 +50,13 @@ return {
         astro = { { 'prettierd', 'prettier' } },
         go = { 'goimports', 'gofumpt' },
       },
-      format_on_save = { async = false, timeout_ms = 500, lsp_fallback = true },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
     }
   end,
 }
